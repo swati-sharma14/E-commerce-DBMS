f263c55fa91a3fa84387b2f7b73b3c9f2104fe31
Swati Sharma <103050981+swati07sharma@users.noreply.github.com>
2023-06-13 01:52:32 +0530

    Delete Ecommerce-DBMS-master directory

--- Ecommerce-DBMS-master/triggers_example.sql
+++
@@ -1,7 +0,0 @@
--- SELECT * FROM products where sellerid = 1;
--- delete from sellers where sellerid = 1;
--- SELECT * FROM products where sellerid = 1;
-
-SELECT * FROM carts where orderid = 1;
-delete from orders where orderid = 1;
-SELECT * FROM carts where orderid = 1;
